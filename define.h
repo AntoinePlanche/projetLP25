ca26dbdca18178f1be323c0559e227e3aaa58408
Thomas <43036801+totordudu@users.noreply.github.com>
2020-05-24 17:45:32 +0200

    add joueur struct

--- define.h
+++ define.h
@@ -18,9 +18,14 @@ int nbrBonneCouleurBonEndroit;
 int nbrBonneCouleurMauvaisEndroit;
 }ligne;
 
-typedef ligne *mastermind; // je sais pas trop à quoi ca peut servir pour l'instant mais on sait jamais
+typedef ligne *mastermind; // je sais pas trop Ã  quoi ca peut servir pour l'instant mais on sait jamais
 
-// il y a 2 grosses fonctions à coder : une fonction qui prend en parametre la ligne "objectif" (juste un tableau de couleur, pas besoin de
+typedef struct joueur{
+  char *nom;
+  int score;
+}joueur;
+
+// il y a 2 grosses fonctions Ã  coder : une fonction qui prend en parametre la ligne "objectif" (juste un tableau de couleur, pas besoin de
 // nbrBonneCouleurBonEndroit et nbrBonneCouleurMauvaisEndroit) et une autre ligne x et qui remplie nbrBonneCouleurBonEndroit, nbrBonneCouleurMauvaisEndroit
 // de la ligne x. Il restera la fonction qui recoit une structure ligne et qui enregistre les information dans un fichier ( ca fera une sorte de sauvegarde pour
 // reprendre la partie si jamais on arrete le programme en cour de partie).
