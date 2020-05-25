b220932cc70b5a24b003f15e3cd34568ed6f5e2d
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-05-25 14:09:10 +0200

    update define.h

--- define.h
+++ define.h
@@ -13,7 +13,8 @@ typedef enum couleur
     VERT,
     BLANC,
     VIOLET,
-    ROSE
+    ROSE,
+    NONE
 } Couleur;
 
 typedef struct ligne
@@ -23,47 +24,58 @@ typedef struct ligne
     int nbrBonneCouleurMauvaisEndroit;
 } ligne;
 
-typedef ligne* mastermind; // je sais pas trop à quoi ca peut servir pour l'instant mais on sait jamais
+typedef ligne *mastermind; // je sais pas trop à quoi ca peut servir pour l'instant mais on sait jamais
 
 typedef struct joueur
 {
     char *nom;
-    ligne proposition;
+    ligne *proposition;
     int score;
 } joueur;
 
-/*
-typedef struct joueur
-{
-    char *nom;
-    masterming proposition;
-    int score;
-} joueur;
+/**
+ * @brief Cette fonction permet de prendre en comtpe les interactions avec le joueur et d'assigner dynamique le tableau de point dans la champ ligne.
+ * 
+ * @param joueur 
+ */
+void playerInput(joueur *joueur);
 
-Je vois plus les choses comme ca perso
-*/
+/**
+ * @brief Cette fonction permet de verifier si le joueur a trouvé ou non la bonne combinaison. Elle retournera un boolean en fonction de la reussite ou non.
+ * 
+ * @param targetLigne 
+ * @param input 
+ * @param lengthLine 
+ * @return true 
+ * @return false 
+ */
+bool checkLineContent(Couleur *targetLigne, ligne *input, int lengthLine);
 
-void playerInput(joueur *joueur); //Cette fonction permet de prendre en comtpe les interactions avec le joueur et d'assigner dynamique le tableau de point dans la champ ligne.
-
-bool checkLineContent(Couleur* targetLigne, ligne* input, int lengthLine); // Cette fonction permet de verifier si le joueur a trouvé ou non la bonne combinaison. Elle retournera un boolean en fonction de la reussite ou non.
-
-//void createGameFile(joueur &joueur); // Cette fonction a pour but de créer un fichier ayant pour nom le nom du joueur et de son adversaire si le fichier existe deja nous demandons au joueur de donner un nom au fichier
-
+/**
+ * @brief Cette fonction a pour but de créer un fichier ayant pour nom le nom du joueur et de son adversaire si le fichier existe deja nous demandons au  joueur de donner un nom au fichier
+ * 
+ * @param joueur 
+ */
 void createGameFile(joueur *joueur);
 
-//void addLigneToFile(ligne &proposition); // Cette fonction a pour but d'ajouter au fichier crée préalablement la ligne que vient de rentrer le joueur ainsi que les nbrBonneCouleurBonEndroit et nbrBonneCouleurMauvaisEndroit.
-
+/**
+ * @brief Cette fonction a pour but d'ajouter au fichier crée préalablement la ligne que vient de rentrer le joueur ainsi que les nbrBonneCouleurBonEndroit et nbrBonneCouleurMauvaisEndroit.
+ * 
+ * @param proposition 
+ */
 void addLigneToFile(ligne *proposition);
 
-//string findFileName(); // Cette fonction a pour objectif de retrouver le nom du fichier dans le dossier Previous-Game.
+/**
+ * @brief  Cette fonction a pour objectif de retrouver le nom du fichier dans le dossier Previous-Game.
+ * 
+ * @return char* 
+ */
+char *findFileName();
 
-char* findFileName();
-
-void createGameFromFile(); // Cette fonction a pour objectif de creer une nouvelle ligne objectif et d'afficher les anciennes decisions qui ont été prises par le joueur afin qu'il se retrouve dans le jeu.
-
-// il y a 2 grosses fonctions à coder : une fonction qui prend en parametre la ligne "objectif" (juste un tableau de couleur, pas besoin de
-// nbrBonneCouleurBonEndroit et nbrBonneCouleurMauvaisEndroit) et une autre ligne x et qui remplie nbrBonneCouleurBonEndroit, nbrBonneCouleurMauvaisEndroit
-// de la ligne x. Il restera la fonction qui recoit une structure ligne et qui enregistre les information dans un fichier ( ca fera une sorte de sauvegarde pour
-// reprendre la partie si jamais on arrete le programme en cour de partie).
+/**
+ * @brief Cette fonction a pour objectif de creer une nouvelle ligne objectif et d'afficher les anciennes decisions qui ont été prises par le joueur afin qu'il se retrouve dans le jeu.
+ * 
+ */
+void createGameFromFile();
 
 #endif // DEFINE_H_INCLUDED
