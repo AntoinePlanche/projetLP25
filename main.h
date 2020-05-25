722646204cec00aa3c190e3fe6488fed273b9e0d
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-05-26 00:10:11 +0200

    delete variables

--- main.h
+++ main.h
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include "define.h"
 
-extern Couleur *combinaison;
-extern int nombreEssais;
-
 /**
  * @brief Cette fonction permet de connaitre le mode de jeu choisi par le joueur 
  * 
