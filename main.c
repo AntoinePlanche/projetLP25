bfbc8c174fca373459d370fe4934b3964feb7855
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-05-28 10:30:05 +0200

    add content to void main

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 
 int main()
 {
+    Couleur combinaison;
+    joueur joueur1;
+
+    joueur1.nom = "Test";
+    joueur1.score = 0;
+
+    numberColorChoice();
+    combinaisonInput(&combinaison);
+
+    playerInput(&joueur1, &combinaison);
 
     return 0;
 }
