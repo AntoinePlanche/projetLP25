effc03fcc4bd3bea3233ddc3511bef71b538218b
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-05-28 10:28:57 +0200

    update include / function declaration

--- main.h
+++ main.h
@@ -1,11 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "define.h"
-
-/**
- * @brief Cette fonction permet de connaitre le mode de jeu choisi par le joueur 
- * 
- * @return true si le JoueurVSJoueur 
- * @return false si JoueurVSOrdinateur
- */
-bool modeChoice();
+#include "playerInput.h"
