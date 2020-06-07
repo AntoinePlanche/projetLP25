50088c9138efeed840a722c0a4d8dffa2d6b9a2c
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-06-07 21:19:33 +0200

    lighter update

--- src/createGameFile.c
+++ src/createGameFile.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include "../include/gestionEnregistrement.h"
 
 char filename[100];
 
-void createGameFile(joueur joueur, char * str){
-
+void createGameFile(joueur joueur, char *str)
+{
     FILE *fp;
-    strcpy(filename, strcat(joueur.nom,".csv"));
-
+    strcpy(filename, strcat(joueur.nom, ".csv"));
 
     fp = fopen(filename, "r+");
-    if(fp != NULL) {
+    if (fp != NULL)
+    {
         fclose(fp);
         printf("Entrez un nom de fichier :");
         scanf("%s", str);
-        strcpy(filename, strcat(str,".csv"));
+        strcpy(filename, strcat(str, ".csv"));
         fp = fopen(filename, "r+");
-        while(fp != NULL){
+        while (fp != NULL)
+        {
             fclose(fp);
             printf("Veuillez saisir un nom de fichier disponible.");
             printf("\nEntrez un nom de fichier :");
             scanf("%s", str);
-            strcpy(filename, strcat(str,".csv"));
+            strcpy(filename, strcat(str, ".csv"));
             fp = fopen(filename, "r+");
         }
     }
