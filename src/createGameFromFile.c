ff5716c2340c09c58c62f33c5c92fde11291ae1e
Paolo <49529621+ShoyenProd@users.noreply.github.com>
2020-06-11 11:16:56 +0200

    add function createGameFromFile (pas terminée)

---
+++ src/createGameFromFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/define.h"
+
+char filename[100];
+
+void createGameFromFile(Couleur *combinaison, joueur joueur)
+{
+    char delimiter[2] = ";";
+    char line
+
+
+    FILE *fp;
+    fp = fopen(filename, "r+");
+
+
+
+    fclose(fp);
+
+}
