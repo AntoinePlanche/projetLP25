cec84a63df307903c6755f37a56a83aff333368e
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-06-11 10:31:53 +0200

    lighter update

--- src/addLigneToFile.c
+++ src/addLigneToFile.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include "../include/gestionEnregistrement.h"
 
 char filename[100];
 int nombreColonne;
 
-void addLigneToFile(ligne *proposition){
+void addLigneToFile(ligne *proposition)
+{
 
     FILE *fp;
     fp = fopen(filename, "a");
     fprintf(fp, "\n");
-    for (int i = 0; i < nombreColonne; i++) {
-        fprintf(fp,"%d", proposition->pion[i]);
+    for (int i = 0; i < nombreColonne; i++)
+    {
+        fprintf(fp, "%d", proposition->pion[i]);
         fprintf(fp, ";");
     }
     fprintf(fp, "%d", proposition->nbrBonneCouleurBonEndroit);
-    //printf("proposition->nbrBonneCouleurBonEndroit : %d", proposition->nbrBonneCouleurBonEndroit);
     fprintf(fp, ";");
     fprintf(fp, "%d", proposition->nbrBonneCouleurMauvaisEndroit);
     fprintf(fp, ";");
