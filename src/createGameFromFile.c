5b528faec16b3f35040b62b07f0b4fe3648cae98
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-06-13 00:58:39 +0200

    modify code

--- src/createGameFromFile.c
+++ src/createGameFromFile.c
@@ -1,126 +1,204 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/define.h"
+#include "../include/playerInput.h"
 #include <string.h>
 #define LINE_LEN 200
 
 char filename[100];
 int essaisRestants;
+int nombreEssais;
+int playerLinesCount;
 
-
+Couleur getColorByIndex(int index);
 
 void addPropToGame(ligne *proposition, int i, int token)
 {
-    printf("Test %i : ", i);
-    printf("%i  =  ", token);
-    if(i == nombreEssais)
+
+    if (i == nombreEssais)
     {
         proposition->nbrBonneCouleurBonEndroit = token;
-        printf("proposition->nbrBonneCouleurBonEndroit : %i\n", proposition->nbrBonneCouleurBonEndroit);
-    } else if(i == nombreEssais + 1) {
-
-        proposition->nbrBonneCouleurMauvaisEndroit = token;
-        printf("proposition->nbrBonneCouleurMauvaisEndroit : %i\n", proposition->nbrBonneCouleurMauvaisEndroit);
-
-    } else {
-        proposition->pion[i] = token;
-        printf("proposition->pion : %i\n", proposition->pion[i]);
+        ASSERT_INT("proposition->nbrBonneCouleurBonEndroit", proposition->nbrBonneCouleurBonEndroit);
     }
-
-    return 0;
-
+    else if (i == nombreEssais + 1)
+    {
+        proposition->nbrBonneCouleurMauvaisEndroit = token;
+        ASSERT_INT("proposition->nbrBonneCouleurMauvaisEndroit", proposition->nbrBonneCouleurMauvaisEndroit);
+    }
+    else
+    {
+        proposition->pion[i] = getColorByIndex(token);
+        ASSERT_INT("proposition->pion", (Couleur)proposition->pion[i]);
+    }
 }
 
+Couleur getColorByIndex(int index)
+{
+    switch (index)
+    {
+    case 0:
+        return ROUGE;
+        break;
+    case 1:
+        return JAUNE;
+        break;
+    case 2:
+        return BLEU;
+        break;
+    case 3:
+        return ORANGE;
+        break;
+    case 4:
+        return VERT;
+        break;
+    case 5:
+        return BLANC;
+        break;
+    case 6:
+        return VIOLET;
+        break;
+    case 7:
+        return ROSE;
+        break;
 
+    default:
+        return NONE;
+        break;
+    }
+}
 
-void parsePlayer(char* line, joueur *joueur, int count, Couleur *combinaison, int countProp)
+void setGameParameters(char *line, joueur *joueur)
 {
     const char delimiter[2] = ";";
-    char* token = strtok(line, delimiter);
+    char *token = strtok(line, delimiter);
     int i = 0;
 
-    printf("count %i\n", count);
+    while (token != NULL)
+    {
+        if (i == 0)
+        {
+            strcpy(joueur->nom, token);
+            ASSERT_DATA("joueur.nom", joueur->nom);
+        }
+        if (i == 1)
+        {
+            nombreColonne = atoi(token);
+            ASSERT_INT("nombreColonne", nombreColonne);
+        }
+        if (i == 2)
+        {
+            nombreEssais = atoi(token);
+            ASSERT_INT("nombreEssais", nombreEssais);
 
+            joueur->proposition = (ligne *)malloc(sizeof(ligne) * nombreEssais);
 
-    if(count != 0 && count != 1)
-       {
-            countProp++;
-       }
-
-
-    while(token != NULL){
-        if(count == 0){
-            if(i == 0){
-                strcpy(joueur->nom, token);
-                printf("joueur.nom = %s\n", joueur->nom);
-
-            }
-            if(i == 1){
-                nombreColonne = atoi(token);
-                printf("nombreColonne = %i\n", nombreColonne);
-
-            }
-            if(i == 2){
-                nombreEssais = atoi(token);
-                printf("nombreEssais = %i\n", nombreEssais);
-
+            if (joueur->proposition == NULL)
+            {
+                ASSERT("ERREUR ALLOCATION MEMOIRE");
+                exit(1);
             }
         }
-        if(count == 1 && i <= nombreColonne-1){
-
-            combinaison[i] = atoi(token);
-            printf("combinaison[%i] = %i\n", i,combinaison[i]);
-
-            }
-
-
-        if(countProp>=0 && i <= nombreColonne+1){
-                addPropToGame(&(joueur->proposition[countProp]), i, atoi(token));
-            }
-
-
-
 
         token = strtok(NULL, delimiter);
         i++;
-
     }
-    return 0;
 }
 
+void fillCombinaisonFromFile(char *line, Couleur *combinaison)
+{
+    const char delimiter[2] = ";";
+    char *token = strtok(line, delimiter);
+    int i = 0;
 
+    while (token != NULL)
+    {
+        if (i <= nombreColonne - 1)
+        {
+            combinaison[i] = getColorByIndex(atoi(token));
+            ASSERT("COMBINAISON");
+        }
+        token = strtok(NULL, delimiter);
+        i++;
+    }
+}
 
-void createGameFromFile(Couleur *combinaison, joueur *joueur)
+void fillInputLine(char *line, ligne *inputLine)
+{
+    const char delimiter[2] = ";";
+    char *token = strtok(line, delimiter);
+    int i = 0;
+
+    while (token != NULL)
+    {
+        if (i == nombreColonne)
+        {
+            inputLine->nbrBonneCouleurBonEndroit = TO_INT(token);
+            ASSERT_INT("proposition->nbrBonneCouleurBonEndroit", inputLine->nbrBonneCouleurBonEndroit);
+        }
+        else if (i == nombreColonne + 1)
+        {
+            inputLine->nbrBonneCouleurMauvaisEndroit = TO_INT(token);
+            ASSERT_INT("proposition->nbrBonneCouleurMauvaisEndroit", inputLine->nbrBonneCouleurBonEndroit);
+        }
+
+        if (i < nombreColonne)
+        {
+            ASSERT("COULEUR");
+            inputLine->pion[i] = getColorByIndex(TO_INT(token));
+            ASSERT_INT("proposition->pion", (Couleur)inputLine->pion[i]);
+        }
+        token = strtok(NULL, delimiter);
+        i++;
+    }
+}
+
+void createGameFromFile(Couleur *combinaison, joueur *player)
 {
     char tampon[20];
     char tampon2[40] = "Previous-Game/";
-    char line[LINE_LEN+1];
+    char line[LINE_LEN + 1];
     int count = 0;
-    int countProp=-1;
+
+    playerLinesCount = 0;
 
     strcpy(tampon, filename);
-    strcat(tampon2,tampon);
+    strcat(tampon2, tampon);
 
-    printf("%s\n", tampon2);
+    ASSERT_DATA("NOM FICHIER", tampon2);
 
     FILE *fp = fopen(tampon2, "r");
 
-    if(fp != NULL)
+    if (fp != NULL)
     {
-
-        printf("\nContents\n");
-        while(fgets(line, LINE_LEN, fp) != NULL)
+        ASSERT("CONTENT");
+        while (fgets(line, LINE_LEN, fp) != NULL)
         {
-            printf("\n%s", line);
-            parsePlayer(line, joueur, count, combinaison, countProp);
+            RETURN;
+            ASSERT_DATA("LINE", line);
+            ASSERT_INT("LINE n°", count);
+
+            if (count == 0)
+            {
+                setGameParameters(line, player);
+                player->proposition = malloc(sizeof(ligne) * nombreEssais);
+            }
+            else if (count == 1)
+                fillCombinaisonFromFile(line, combinaison);
+            else
+            {
+                ligne inputLine;
+                inputLine.pion = (Couleur *)malloc(sizeof(Couleur) * nombreColonne);
+
+                fillInputLine(line, &inputLine);
+
+                player->proposition[playerLinesCount] = inputLine;
+
+                playerLinesCount++;
+            }
+
             count++;
         }
-
     }
 
     essaisRestants = nombreEssais - count + 2;
-
-
-
-    return 0;
 }
