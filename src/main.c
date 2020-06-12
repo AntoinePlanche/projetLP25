af5945e164c7a44b21b078d69465f44a885cbba8
thomas.duvinage <thomas.duvinage@utbm.fr>
2020-06-13 00:59:23 +0200

    lighter update

--- src/main.c
+++ src/main.c
@@ -20,127 +20,119 @@ int main()
     srand(time(NULL));
     joueur *nouveauJoueur = malloc(sizeof(joueur));
     printf("Bonjour, quel est votre nom ?\n");
-    scanf("%s",nouveauJoueur->nom);
-    Couleur *combinaisonSecret = (Couleur*)malloc(nombreColonne * sizeof(Couleur));
-
-
+    scanf("%s", nouveauJoueur->nom);
+    Couleur *combinaisonSecret = (Couleur *)malloc(nombreColonne * sizeof(Couleur));
 
     if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
         {
-            while ((dir = readdir(d)) != NULL)
-            {
-                isCSVFile = strstr(dir->d_name, ".csv");
+            isCSVFile = strstr(dir->d_name, ".csv");
 
-                if(isCSVFile != NULL) {
-                    nbFileCSV = nbFileCSV + 1;
+            if (isCSVFile != NULL)
+            {
+                nbFileCSV = nbFileCSV + 1;
+            }
+        }
+        closedir(d);
+        if (nbFileCSV != 0)
+        {
+
+            printf("Voulez-vous reprendre une partie en cours ? (o/n)\n");
+            scanf(" %c", &choixReprendrePartie);
+            while (choixReprendrePartie != 'o' && choixReprendrePartie != 'O' && choixReprendrePartie != 'n' && choixReprendrePartie != 'N')
+            {
+                printf("Voulez-vous reprendre une partie en cours ? (o/n)\n");
+                scanf(" %c", &choixReprendrePartie);
+            }
+
+            if (choixReprendrePartie == 'o' || choixReprendrePartie == 'O')
+            {
+
+                findFileName();
+
+                createGameFromFile(combinaisonSecret, nouveauJoueur);
+
+                numeroEssai = nombreEssais - essaisRestants;
+
+                printf("COMBINAISON : \n");
+                displayChoice(combinaisonSecret);
+
+                printf("ANCIENS CHOIX : \n");
+                for (int i = 0; i < playerLinesCount; i++)
+                {
+                    RETURN;
+                    ASSERT_INT("LIGNE N°", i);
+                    displayChoice(nouveauJoueur->proposition[i].pion);
+
+                    ASSERT_INT("MAUVAISE REPONSE", nouveauJoueur->proposition[i].nbrBonneCouleurMauvaisEndroit);
+
+                    ASSERT_INT("BON REPONSE", nouveauJoueur->proposition[i].nbrBonneCouleurBonEndroit);
+                }
+            }
+            else
+            {
+                createGameFile(*nouveauJoueur, str);
+
+                nouveauJoueur->score = 0;
+                essaisNumberChoice();
+                numberColorChoice();
+                nouveauJoueur->proposition = (ligne *)malloc(sizeof(ligne) * nombreEssais);
+                if (nouveauJoueur->proposition == NULL)
+                {
+                    printf("erreur d'allocation memoire");
+                    exit(1);
                 }
 
+                for (int i = 0; i < nombreEssais; i++)
+                {
+                    nouveauJoueur->proposition[i].pion = (Couleur *)malloc(sizeof(Couleur) * nombreColonne);
 
-            }
-            closedir(d);
-            if(nbFileCSV != 0){
-
-
-                    printf("Voulez-vous reprendre une partie en cours ? (o/n)\n");
-                    scanf(" %c", &choixReprendrePartie);
-                    while(choixReprendrePartie != 'o' && choixReprendrePartie != 'O' && choixReprendrePartie != 'n' && choixReprendrePartie != 'N')
+                    if (nouveauJoueur->proposition[i].pion == NULL)
                     {
-                        printf("Voulez-vous reprendre une partie en cours ? (o/n)\n");
-                        scanf(" %c", &choixReprendrePartie);
-                    }
-
-                    if(choixReprendrePartie == 'o' || choixReprendrePartie == 'O')
-                    {
-
-                        findFileName();
-
-                        nouveauJoueur->proposition = (ligne*)malloc(sizeof(ligne)*nombreEssais);
-
-                        nouveauJoueur->score = 0;
-                        createGameFromFile(combinaisonSecret, nouveauJoueur);
-                        numeroEssai = nombreEssais - essaisRestants;
-                        printf("numeroEssai = %i\n", numeroEssai);
-
-                    } else {
-
-                        createGameFile(*nouveauJoueur, str);
-
-                        nouveauJoueur->score = 0;
-                        essaisNumberChoice();
-                        numberColorChoice();
-                        nouveauJoueur->proposition = (ligne*)malloc(sizeof(ligne)*nombreEssais);
-                        if (nouveauJoueur->proposition == NULL)
-                        {
-                            printf("erreur d'allocation memoire");
-                            exit(1);
-                        }
-
-                        for(int i = 0; i<nombreEssais; i++)
-                        {
-                            nouveauJoueur->proposition[i].pion = (Couleur*)malloc(sizeof(Couleur)*nombreColonne);
-                            if(nouveauJoueur->proposition[i].pion == NULL)
-                            {
-                                printf("erreur d'allocation memoire\n");
-                                exit(1);
-                            }
-                        }
-
-
-                        combinaisonInput(combinaisonSecret, nouveauJoueur);
-
-
-
-
-
-                    }
-
-
-                } else {
-
-                    createGameFile(*nouveauJoueur, str);
-
-                    nouveauJoueur->score = 0;
-                    essaisNumberChoice();
-                    numberColorChoice();
-                    nouveauJoueur->proposition = (ligne*)malloc(sizeof(ligne)*nombreEssais);
-                    if (nouveauJoueur->proposition == NULL)
-                    {
-                        printf("erreur d'allocation memoire");
+                        printf("erreur d'allocation memoire\n");
                         exit(1);
                     }
-
-                    for(int i = 0; i<nombreEssais; i++)
-                    {
-                        nouveauJoueur->proposition[i].pion = (Couleur*)malloc(sizeof(Couleur)*nombreColonne);
-                        if(nouveauJoueur->proposition[i].pion == NULL)
-                        {
-                            printf("erreur d'allocation memoire\n");
-                            exit(1);
-                        }
-                    }
-
-
-                    combinaisonInput(combinaisonSecret, nouveauJoueur);
-
-
                 }
 
-
+                combinaisonInput(combinaisonSecret, nouveauJoueur);
+            }
         }
+        else
+        {
 
+            createGameFile(*nouveauJoueur, str);
 
+            nouveauJoueur->score = 0;
+            essaisNumberChoice();
+            numberColorChoice();
+            nouveauJoueur->proposition = (ligne *)malloc(sizeof(ligne) * nombreEssais);
+            if (nouveauJoueur->proposition == NULL)
+            {
+                printf("erreur d'allocation memoire");
+                exit(1);
+            }
 
+            for (int i = 0; i < nombreEssais; i++)
+            {
+                nouveauJoueur->proposition[i].pion = (Couleur *)malloc(sizeof(Couleur) * nombreColonne);
+                if (nouveauJoueur->proposition[i].pion == NULL)
+                {
+                    printf("erreur d'allocation memoire\n");
+                    exit(1);
+                }
+            }
 
+            combinaisonInput(combinaisonSecret, nouveauJoueur);
+        }
+    }
 
-    for(numeroEssai; numeroEssai<nombreEssais; numeroEssai++)
+    for (int i = 0; i < nombreEssais; i++)
     {
-        playerInput(nouveauJoueur, combinaisonSecret, numeroEssai);
-
+        playerInput(nouveauJoueur, combinaisonSecret, i);
     }
     //remove(strcat("Previous-Game/",filename));
-    printf("Dommage %s, vous avez perdu, n'hesitez pas a reessayer.\n",nouveauJoueur->nom);
-
-
+    printf("Dommage %s, vous avez perdu, n'hesitez pas a reessayer.\n", nouveauJoueur->nom);
 
     return 0;
 }
