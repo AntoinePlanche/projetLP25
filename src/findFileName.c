1b55852e7cfb7ab200178e3fa0b4a6d782f1810d
Paolo <49529621+ShoyenProd@users.noreply.github.com>
2020-06-14 14:00:56 +0200

    add delete function

--- src/findFileName.c
+++ src/findFileName.c
@@ -5,6 +5,105 @@
 #include "../include/gestionEnregistrement.h"
 
 /**
+ * @brief This method is used to delete a file
+ * 
+ * @return void 
+ */
+void deleteFile()
+{
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("./Previous-Game");
+    char playerAnswer, choixFile[100], *testCSVFile, folder[20]= "Previous-Game/";
+
+
+    printf("Voulez vous supprimer un fichier ? (o/n)\n");
+    scanf(" %c", &playerAnswer);
+    while (playerAnswer != 'o' && playerAnswer != 'O' && playerAnswer != 'n' && playerAnswer != 'N')
+    {
+        printf("Votre reponse doit etre (o/n)\n");
+        scanf(" %c", &playerAnswer);
+    }
+    if (playerAnswer == 'O' || playerAnswer == 'o')
+    {
+        d = opendir("./Previous-Game");
+        printf("Voici la liste des fichiers de sauvegarde :\n");
+        if (d)
+        {
+            while ((dir = readdir(d)) != NULL)
+            {
+                testCSVFile = strstr(dir->d_name, ".csv");
+
+                if (testCSVFile != NULL)
+                {
+                    printf("- %s\n", dir->d_name);
+                }
+            }
+            closedir(d);
+        }
+
+        d = opendir("./Previous-Game");
+        bool testFind = false;
+        printf("Quel fichier voulez-vous supprimer ?\n");
+        scanf("%s", choixFile);
+        if (d)
+        {
+            while ((dir = readdir(d)) != NULL)
+            {
+                testCSVFile = strstr(dir->d_name, ".csv");
+
+                if (testCSVFile != NULL && strcmp(choixFile, dir->d_name) == 0)
+                {
+                    strcpy(filename, dir->d_name);
+                    closedir(d);
+                    remove(strcat(folder,filename));
+                    printf("Fichier supprimé \n");
+                    return deleteFile();
+                }
+                else
+                {
+                    testFind = true;
+                }
+            }
+            closedir(d);
+        }
+
+        while (testFind)
+        {
+            printf("Erreur ! Merci de saisir un nom de fichier correct !\n");
+            printf("Quel fichier voulez-vous supprimer ?\n");
+            scanf("%s", choixFile);
+            d = opendir("./Previous-Game");
+            if (d)
+            {
+
+                while ((dir = readdir(d)) != NULL)
+                {
+                    testCSVFile = strstr(dir->d_name, ".csv");
+
+                    if (testCSVFile != NULL && strcmp(choixFile, dir->d_name) == 0)
+                    {
+                        strcpy(filename, dir->d_name);
+                        closedir(d);
+                        remove(strcat(folder,filename));
+                        printf("Fichier supprimé \n");
+                        return deleteFile();
+                    }
+                    else
+                    {
+                        testFind = true;
+                    }
+                }
+                closedir(d);
+            }
+        }
+
+    }
+    return 0;
+}
+
+
+/**
  * @brief This method is used to find File Name
  * 
  * @return void* 
